1a9c2f1fc4c44a66762b07104d6aa75713e6dbbe
victor medrado <victor@agenciaexplorer.com.br>
2024-01-30 12:39:40 -0300

    no index

--- include/helmet.php
+++ include/helmet.php
@@ -6,6 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <meta name="robots" content="noindex">
+
   <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
 
   <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.min.css">
